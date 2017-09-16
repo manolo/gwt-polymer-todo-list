1c40511963f08acbca63dda54fa5f91decc7fe16
Manolo Carrasco <manolo@vaadin.com>
2017-09-17 00:38:11 +0200

    Remove widget references in elements tutorial

--- elements-todo-list/src/main/java/org/gwtproject/tutorial/client/Main.java
+++ elements-todo-list/src/main/java/org/gwtproject/tutorial/client/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
